f5dcf86640afb30a2d72ecc3d3d3b67fad7df601
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-01-24 21:28:21 +0900

    update version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ if major == 2 and minor <= 5:
 
 setuptools.setup(
     name="thutils",
-    version="0.1.13",
+    version="0.1.14",
     author="Tsuyoshi Hombashi",
     author_email="gogogo.vm@gmail.com",
     url="https://github.com/thombashi/thutils",
