d8ba9f1f05282bdf4c1cfd45caa392d68bbeeeb1
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-01-27 01:47:46 +0900

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,8 @@
 import sys
 import os
 
+import sphinx_rtd_theme
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -51,8 +53,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'thutils'
-copyright = u'2016, Author'
-author = u'Author'
+copyright = u'2016, Tsuyoshi Hombashi'
+author = u'Tsuyoshi Hombashi'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -112,7 +114,8 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -121,6 +124,7 @@ html_theme = 'alabaster'
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
