3808b100f4d55a62e3a03e027550b37445a49614
Tsuyoshi Hombashi <gogogo.vm@gmail.com>
2016-08-27 22:49:59 +0900

    Remove function that no longer used

--- thutils/common.py
+++ thutils/common.py
@@ -90,33 +90,6 @@ def bytes_to_humanreadable(byte):
     return value + " " + unit
 
 
-def split_line_list(
-        line_list, re_line_separator=re.compile("^$"),
-        is_include_matched_line=False, is_strip=True):
-    block_list = []
-    block = []
-
-    for line in line_list:
-        if is_strip:
-            line = line.strip()
-
-        if re_line_separator.search(line):
-            if len(block) > 0:
-                block_list.append(block)
-
-            block = []
-            if is_include_matched_line:
-                block.append(line)
-            continue
-
-        block.append(line)
-
-    if len(block) > 0:
-        block_list.append(block)
-
-    return block_list
-
-
 def is_install_command(command):
     import subprocess
     import platform
